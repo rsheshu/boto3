c0d4f7e10faabb6cddb256682d029022ab4bcddd
Michael Gibbons <migibbons@BELC02MR5T2F6T6.sea.corp.expecn.com>
2016-05-15 22:38:45 -0700

    Updated script, added new functions

--- s3_bucket.py
+++ s3_bucket.py
@@ -1,40 +1,74 @@
 #!/usr/bin/env python
 
-import boto3, argparse, os
+import boto3, argparse
 from subprocess import call
-
-#session = boto3.Session('AKIAI6H266XCWYNBCDVQ', 'N8KhjfE3zMA3lf7jFBOUGrDRpK1P18RaU2jR276V')
+from os import path
 
 arg_parser = argparse.ArgumentParser(description='Script to upload files file to S3 bucket')
-arg_parser.add_argument('-l', '--list', help='List the contents of the bucket', action="store_true")
-arg_parser.add_argument('-u', '--file', help='Specify a file to upload. Must specify destination as well: \'-d/--destination\'')
-arg_parser.add_argument('-D', '--destination', help='Specify the destination directory to upload files to')
-arg_parser.add_argument('-d', '--directory', help='Upload the contents of a directory. Must specify destination as well: \'-d/--destination\'')
+arg_parser.add_argument('--list', help='List the contents of the bucket', action="store_true")
+arg_parser.add_argument('--upload', help='Specify a file to upload to S3. Must specify destination as well: \'-d/--destination\'')
+arg_parser.add_argument('--download', help='Specify a file to download from S3. Must specify destination as well: \'-d/--destination\'')
+arg_parser.add_argument('--upload-dir', help='Upload the contents of a directory to S3. Must specify destination as well: \'-d/--destination\'')
+arg_parser.add_argument('--download-dir', help='Downloads the contents of a directory from S3. Must specify destination as well: \'-d/--destination\'')
+arg_parser.add_argument('--destination', help='Specify the destination directory to upload files to')
 args = arg_parser.parse_args()
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('gibb-bucket')
+path = os.path()
 
+'''
+Add section to check for .config and .creds section?
+'''
+
+def if_location_exists(location):
+    try:
+        path.exists(location)
+    except IOError:
+        print "File or directory does not exist"
+'''
+Need to add destination dir to list
+'''
 def list_bucket():
     for obj in bucket.objects.all():
         print obj.key
 
 def upload_file():
-    files = args.file
-    file_basename = os.path.basename(files)
-    destination = os.path.normpath(args.destination)
-    s3.meta.client.upload_file(files, 'gibb-bucket', os.path.join(destination, file_basename))
+    files = args.upload
+    file_basename = path.basename(files)
+    destination = path.normpath(args.destination)
+    if_location_exists(files)
+    s3.meta.client.upload_file(files, str(bucket), path.join(destination, file_basename))
+
+def download_file():
+    files = args.download
+    file_basename = path.basename(files)
+    destination = path.normpath(args.destination)
+    s3.meta.client.download_file(files, str(bucket), path.join(destination, file_basename))
 
 def upload_directory():
-    directory = os.path.normpath(args.directory)
-    destination = os.path.normpath(args.destination)
-    call(['aws', 's3', 'cp', '--recursive', directory, 's3://gibb-bucket/' + str(destination)])
+    directory = path.normpath(args.upload-dir)
+    destination = path.normpath(args.destination)
+    if_location_exists(directory)
+    call(['aws', 's3', 'cp', '--recursive', directory, 's3://' + str(bucket) + '/' + str(destination)])
 
+def download_directory():
+    directory = path.normpath(args.download_dir)
+    destination = path.normpath(args.destination)
+    if_location_exists(destination)
+    call(['aws', 's3', 'cp', '--recursive', 's3://' + str(bucket) + '/' + str(directory), str(destination)])
 
+'''
+Cleaner way of doing this?
+'''
 if __name__ == '__main__':
     if args.list:
         list_bucket()
-    if args.file:
+    if args.upload:
         upload_file()
-    if args.directory:
+    if args.upload-dir:
         upload_directory()
+    if args.upload:
+        download_file()
+    if args.upload-dir:
+        download_directory()
